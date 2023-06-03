054ce8350ebf75a6f3155e88bee40a48e536809f
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-06-03 17:01:08 +0200

    add modell Student

---
+++ src/MvcProject.WebApplication/Models/Student.cs
@@ -0,0 +1,9 @@
+namespace Spg.ProjectManager.Application.Models
+{
+    public class Student : EntityBase
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+    }
+}
