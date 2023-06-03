a1f36fab57d5d402bd051469faa0970b60f87c3b
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-06-03 17:00:29 +0200

    add modell Teacher

---
+++ src/MvcProject.WebApplication/Models/Teacher.cs
@@ -0,0 +1,12 @@
+namespace Spg.ProjectManager.Application.Models
+{
+    public class Teacher : EntityBase
+    {
+        public string? Firstname { get; set; }
+
+        public string? Lastname { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
