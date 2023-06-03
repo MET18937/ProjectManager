ae559ce6c297edde3ae87abff04ca5c1703154f8
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-06-03 17:24:38 +0200

    change supervisor ForeignKey

--- src/MvcProject.WebApplication/Models/Supervisor.cs
+++ src/MvcProject.WebApplication/Models/Supervisor.cs
@@ -4,9 +4,8 @@ namespace Spg.ProjectManager.Application.Models
 {
     public class Supervisor : EntityBase
     {
-        // foreign key from table teacher
-        [ForeignKey("TeacherId")]
-        public Teacher? TeacherId { get; set; }
+        [ForeignKey("Teacher")]
+        public int? TeacherId { get; set; }
         public string? Description { get; set; }
 
     }
