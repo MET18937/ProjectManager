1b992880ac564aca461fe1cc29f81f4b1469de3e
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-06-03 17:27:08 +0200

    change modell Project ForeignKey

--- src/MvcProject.WebApplication/Models/Project.cs
+++ src/MvcProject.WebApplication/Models/Project.cs
@@ -7,11 +7,11 @@ namespace Spg.ProjectManager.Application.Models
 		public string? Title { get; set; }
 		public string? Description { get; set; }
 		public DateTime SubmitDate { get; set; }
-		[ForeignKey("CompanyId")]
-		public Company? CompanyId { get; set; }
-		[ForeignKey("TeacherId")]
-		public Teacher? TeacherId { get; set; }
-		[ForeignKey("SupervisorId")]
-		public Supervisor? SupervisorId { get; set; }
-	}
+        [ForeignKey("Supervisor")]
+        public int? SupervisorId { get; set; }
+        [ForeignKey("Company")]
+		public int? CompanyId { get; set; }
+        [ForeignKey("Teacher")]
+        public int? TeacherId { get; set; }
+    }
 }
